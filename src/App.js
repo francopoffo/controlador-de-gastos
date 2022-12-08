ea1f52bfccb5df06f45c3917db9679cd344f95e6
francopoffo <franco_sutter@yahoo.com.br>
2022-12-08 13:15:30 -0300

    Criando componente de filtro

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
+import React from "react";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+// import { useState } from "react";
 
 function App() {
   const expenses = [
     {
       id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
+      title: "Ração do gato",
+      amount: 45.23,
+      date: new Date(2022, 7, 14),
     },
     {
       id: "e2",
-      title: "New TV",
-      amount: 799.49,
-      date: new Date(2021, 2, 12),
+      title: "Diarista",
+      amount: 110.0,
+      date: new Date(2022, 2, 12),
     },
     {
       id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
+      title: "Notebook",
+      amount: 4750.0,
+      date: new Date(2022, 12, 28),
     },
     {
       id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
+      title: "Conta de luz",
+      amount: 150,
+      date: new Date(2023, 1, 12),
     },
   ];
 
+  // const [updatedExpenses, setUpdatedExpenses] = useState(expenses);
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense)
+  };
+  
+
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
